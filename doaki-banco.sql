3cdd16e1651af23ceb3b31d0b4f5a75a347b70f2
Brenda Miranda <brendatw43@gmail.com>
2023-10-30 21:48:26 -0300

    alteração no banco de dados

--- doaki-banco.sql
+++ doaki-banco.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: doaki
 -- ------------------------------------------------------
--- Server version	8.0.35
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -42,6 +42,62 @@ INSERT INTO `categoria` VALUES (1,'graos',NULL),(2,'liquido',NULL),(3,'cereais',
 UNLOCK TABLES;
 
 --
+-- Table structure for table `doacao`
+--
+
+DROP TABLE IF EXISTS `doacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doacao` (
+  `doacao_id` int NOT NULL AUTO_INCREMENT,
+  `doador_id` bigint NOT NULL,
+  `usuario_id` bigint NOT NULL,
+  `data_hora_doado` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`doacao_id`),
+  KEY `fk_doacao_doador` (`doador_id`),
+  KEY `fk_doacao_usuario` (`usuario_id`),
+  CONSTRAINT `fk_doacao_doador` FOREIGN KEY (`doador_id`) REFERENCES `doador` (`doador_id`),
+  CONSTRAINT `fk_doacao_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doacao`
+--
+
+LOCK TABLES `doacao` WRITE;
+/*!40000 ALTER TABLE `doacao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doacao_item`
+--
+
+DROP TABLE IF EXISTS `doacao_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doacao_item` (
+  `doacao_item_id` int NOT NULL,
+  `item_id` bigint NOT NULL,
+  `quantidade` int NOT NULL,
+  KEY `fk_doacao_item_doacao` (`doacao_item_id`),
+  KEY `fk_doacao_item_item` (`item_id`),
+  CONSTRAINT `fk_doacao_item_doacao` FOREIGN KEY (`doacao_item_id`) REFERENCES `doacao` (`doacao_id`),
+  CONSTRAINT `fk_doacao_item_item` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doacao_item`
+--
+
+LOCK TABLES `doacao_item` WRITE;
+/*!40000 ALTER TABLE `doacao_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doacao_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `doador`
 --
 
@@ -49,11 +105,11 @@ DROP TABLE IF EXISTS `doador`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `doador` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `doador_id` bigint NOT NULL AUTO_INCREMENT,
   `nome` varchar(75) NOT NULL,
   `cpf` varchar(20) NOT NULL,
   `telefone` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`doador_id`),
   UNIQUE KEY `cpf` (`cpf`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -75,11 +131,11 @@ DROP TABLE IF EXISTS `item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `item` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `item_id` bigint NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) NOT NULL,
   `quantidade` bigint NOT NULL,
   `categoria_id` int NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`item_id`),
   UNIQUE KEY `nome` (`nome`),
   KEY `fk_item_categoria` (`categoria_id`),
   CONSTRAINT `fk_item_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`)
@@ -103,13 +159,13 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
+  `usuario_id` bigint NOT NULL AUTO_INCREMENT,
   `nome` varchar(75) NOT NULL,
   `perfil` varchar(20) NOT NULL DEFAULT 'PADRAO',
   `usuario` varchar(50) NOT NULL,
   `senha` varchar(100) NOT NULL,
   `estado` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`usuario_id`),
   UNIQUE KEY `usuario` (`usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -140,4 +196,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-30  0:17:29
+-- Dump completed on 2023-10-30 21:43:45
