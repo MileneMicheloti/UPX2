9b2b2428698fbb4e9156c5b2db1c2698e328ae14
Brenda Miranda <brendatw43@gmail.com>
2023-11-02 23:11:26 -0300

    Ajustando merge do banco

--- Banco_UPX.sql
+++ Banco_UPX.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `doaki` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `doaki`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: doaki
 -- ------------------------------------------------------
--- Server version	8.0.33
+-- Server version	8.0.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,10 +27,10 @@ DROP TABLE IF EXISTS `categoria`;
 CREATE TABLE `categoria` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
-  `descricao` varbinary(200) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nome` (`nome`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +39,7 @@ CREATE TABLE `categoria` (
 
 LOCK TABLES `categoria` WRITE;
 /*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-INSERT INTO `categoria` VALUES (1,'graos',NULL),(2,'liquido',NULL),(3,'cereais',NULL),(4,'po',NULL);
+INSERT INTO `categoria` VALUES (1,'po',NULL),(2,'graos',NULL),(3,'cereais',NULL);
 /*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,15 +51,15 @@ DROP TABLE IF EXISTS `doacao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `doacao` (
-  `doacao_id` int NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `doador_id` bigint NOT NULL,
   `usuario_id` bigint NOT NULL,
   `data_hora_doado` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`doacao_id`),
+  PRIMARY KEY (`id`),
   KEY `fk_doacao_doador` (`doador_id`),
   KEY `fk_doacao_usuario` (`usuario_id`),
-  CONSTRAINT `fk_doacao_doador` FOREIGN KEY (`doador_id`) REFERENCES `doador` (`doador_id`),
-  CONSTRAINT `fk_doacao_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`)
+  CONSTRAINT `fk_doacao_doador` FOREIGN KEY (`doador_id`) REFERENCES `doador` (`id`),
+  CONSTRAINT `fk_doacao_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -78,13 +80,13 @@ DROP TABLE IF EXISTS `doacao_item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `doacao_item` (
-  `doacao_item_id` int NOT NULL,
+  `doacao_id` bigint NOT NULL,
   `item_id` bigint NOT NULL,
   `quantidade` int NOT NULL,
-  KEY `fk_doacao_item_doacao` (`doacao_item_id`),
+  KEY `fk_doacao_item_doacao` (`doacao_id`),
   KEY `fk_doacao_item_item` (`item_id`),
-  CONSTRAINT `fk_doacao_item_doacao` FOREIGN KEY (`doacao_item_id`) REFERENCES `doacao` (`doacao_id`),
-  CONSTRAINT `fk_doacao_item_item` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`)
+  CONSTRAINT `fk_doacao_item_doacao` FOREIGN KEY (`doacao_id`) REFERENCES `doacao` (`id`),
+  CONSTRAINT `fk_doacao_item_item` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -105,11 +107,11 @@ DROP TABLE IF EXISTS `doador`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `doador` (
-  `doador_id` bigint NOT NULL AUTO_INCREMENT,
-  `nome` varchar(75) NOT NULL,
-  `cpf` varchar(20) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `cpf` varchar(25) NOT NULL,
   `telefone` varchar(20) NOT NULL,
-  PRIMARY KEY (`doador_id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `cpf` (`cpf`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -131,11 +133,11 @@ DROP TABLE IF EXISTS `item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `item` (
-  `item_id` bigint NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) NOT NULL,
-  `quantidade` bigint NOT NULL,
+  `quantidade` int NOT NULL,
   `categoria_id` int NOT NULL,
-  PRIMARY KEY (`item_id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `nome` (`nome`),
   KEY `fk_item_categoria` (`categoria_id`),
   CONSTRAINT `fk_item_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`)
@@ -159,13 +161,13 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `usuario_id` bigint NOT NULL AUTO_INCREMENT,
-  `nome` varchar(75) NOT NULL,
-  `perfil` varchar(20) NOT NULL DEFAULT 'PADRAO',
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `senha` varchar(100) NOT NULL,
-  `estado` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`usuario_id`),
+  `perfil` varchar(10) NOT NULL DEFAULT 'PADRAO',
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `usuario` (`usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -182,10 +184,6 @@ UNLOCK TABLES;
 --
 -- Dumping events for database 'doaki'
 --
-
---
--- Dumping routines for database 'doaki'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -196,4 +194,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-30 21:43:45
+-- Dump completed on 2023-11-02 19:35:53
